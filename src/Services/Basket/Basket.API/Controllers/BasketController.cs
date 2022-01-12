35adc542aa2344f262b252bb259529124f74104f
Gigi Zurabashvili <gigilozurabashvili@gmail.com>
2022-01-12 19:06:52 +0400

    Added API template for Basket API

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get()
+        {
+            string text = await Task.FromResult<string>("Welcome to basket Api");
+            return Ok(text);
+        }
+    }
+}
