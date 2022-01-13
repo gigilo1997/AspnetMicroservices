5467cd4f3db1f4ddc73aa13bc44d74f5dfa7020b
Gigi Zurabashvili <gigilozurabashvili@gmail.com>
2022-01-14 01:41:40 +0400

    Implemented Discount Grpc service

---
+++ src/Services/Discount/Discount.Grpc/Profiles/DiscountProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Profiles;
+
+public class DiscountProfile : Profile
+{
+    public DiscountProfile()
+    {
+        CreateMap<Coupon, CouponModel>().ReverseMap();
+    }
+}
